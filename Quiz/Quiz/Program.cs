bceb37620a12fb69d46815c8eaee9023c1a56514
andrzej-herman <aherman@san.edu.pl>
2022-10-30 14:58:25 +0100

    30.10.2022

---
+++ Quiz/Quiz/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine();
+
+// 
+
+
+Console.WriteLine();
+
+//// liczby całkowite
+//byte a = 3;
+//short b = 345;
+//int x = 5;
+//long c = 5453453453453453;
+
+//// liczby zmiennoprzecinkowe
+//decimal d = 3.14m;
+//float f = 34.67f;
+//double g = 45.54554545454545;
+
+//// typy nieliczbowe
+//string h = "Ala";
+//bool i = true;
+//DateTime j;
+
+
+
+
