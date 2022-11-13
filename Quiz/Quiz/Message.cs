eb2fdf85b2ed3d44337aecb521ff2715c13b25fa
andrzej-herman <aherman@san.edu.pl>
2022-11-13 14:57:23 +0100

    13.11.2022

---
+++ Quiz/Quiz/Message.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz
+{
+    public class Message
+    {
+        public void DisplayWelcomeScreen()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Witamy w Quizie gr 4");
+            Console.WriteLine("Spróbuj opowiedzieć na 7 pytań");
+            Console.WriteLine("POWODZENIA !!!");
+            Console.WriteLine();
+            Console.WriteLine("Naciśnij ENTER aby rozpocząc grę ...");
+            Console.ReadLine();
+        }
+    }
+}
--- Quiz/Quiz/Program.cs
+++ Quiz/Quiz/Program.cs
@@ -1,26 +1,8 @@
-﻿Console.WriteLine();
+﻿using Quiz;
 
-// 
+var game = new Game();  
+var message = new Message();
+message.DisplayWelcomeScreen();
 
-
-Console.WriteLine();
-
-//// liczby całkowite
-//byte a = 3;
-//short b = 345;
-//int x = 5;
-//long c = 5453453453453453;
-
-//// liczby zmiennoprzecinkowe
-//decimal d = 3.14m;
-//float f = 34.67f;
-//double g = 45.54554545454545;
-
-//// typy nieliczbowe
-//string h = "Ala";
-//bool i = true;
-//DateTime j;
-
-
-
-
+// konstruktor
+// dalej => tworzenie bazy pytań
