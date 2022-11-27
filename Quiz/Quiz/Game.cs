c0925a46aa1dc4f7b31d9c72b80372ef2e68d274
andrzej-herman <aherman@san.edu.pl>
2022-11-27 14:59:30 +0100

    27.11.2022

--- Quiz/Quiz/Game.cs
+++ Quiz/Quiz/Game.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,39 @@ namespace Quiz
 {
     public class Game
     {
+        public Game()
+        {
+            CreateQuestions();
+            CurrentCategory = 100;
+            Random = new Random();
+        }
+
+
+        public List<Question> Questions { get; set; }
+        public int CurrentCategory { get; set; }
+        public Random Random { get; set; }
+        public Question CurrentQuestion { get; set; }
+
+        private void CreateQuestions()
+        {
+            var path = Directory.GetCurrentDirectory() + "\\questions.json";;
+            var text = File.ReadAllText(path);
+            Questions = JsonConvert.DeserializeObject<List<Question>>(text);
+        }
+
+        public void GetQuestion()
+        {
+            var qC = new List<Question>();
+            foreach (var question in Questions)
+            {
+                if (question.Category == CurrentCategory)
+                {
+                    qC.Add(question);   
+                }
+            }
+
+            var number = Random.Next(0, qC.Count);
+            CurrentQuestion = qC[number];
+        }
     }
 }
--- Quiz/Quiz/Program.cs
+++ Quiz/Quiz/Program.cs
@@ -1,8 +1,26 @@
 ﻿using Quiz;
 
-var game = new Game();  
+
+
+
+// tworzenie obiektu typu Gra
+var game = new Game();
+
+// tworzenie obiektu typu Message
 var message = new Message();
+
+// wyswietlanie powitania
 message.DisplayWelcomeScreen();
 
-// konstruktor
-// dalej => tworzenie bazy pytań
+// losowanie pytania z aktualnej kategorii
+game.GetQuestion();
+
+Console.WriteLine(game.CurrentQuestion.Content);
+Console.WriteLine();
+Console.WriteLine();
+
+//konstruktor
+//dalej => tworzenie bazy pytań
+// wyjaśnić namespace
+
+
